b7762d7f9b7777ee378424cfdf31f7d52485a708
RonyFrias <ronyfreezer@gmail.com>
2019-06-05 12:24:53 -0400

    JS

---
+++ javascript_101/0_script.js
@@ -0,0 +1,38 @@
+alert("Hola a todos");
+
+/* Pedir al usuario que ingrese su edad */
+
+// Soy un comentario de 1 sola linea
+
+// pedir al usuario que digite su nombre
+var nombre = prompt("Digite su Nombre");
+
+var edad = prompt("Digite su Edad");
+
+var ciudad = prompt("Digite su Ciudad");
+
+var tarjeta_credito = prompt("Digite su Tarjeta de Credito");
+
+// Imprimir los valores por la consola
+console.log(nombre);
+console.log(edad);
+console.log(ciudad);
+console.log(tarjeta_credito);
+
+console.log(document.documentURI);
+console.log(document.body);
+console.log(document.head);
+console.log(document.height);
+console.log(document.URL);
+console.log(document.title);
+
+// Esta variable no esta definida
+var pais;
+console.log(pais);
+
+// La variable esta definida
+var pais = "Rep. Dom"
+console.log(pais);
+
+// Cambiar el titulo
+document.title = "Nuevo titulo";
---
+++ javascript_101/1_ejercicio.js
@@ -0,0 +1,6 @@
+// Hacer un script que el usuario
+var nombre = prompt("Digite su nombre");
+var edad = prompt("Digite su edad");
+
+document.write(`<h1>${ronyf}</h1>`);
+document.write(`<p>${19}</p>`);
