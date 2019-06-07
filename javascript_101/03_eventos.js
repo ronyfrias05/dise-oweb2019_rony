1ac8a2db539c58603b35221090b853305e7c8dac
RonyFrias <ronyfreezer@gmail.com>
2019-06-07 11:27:05 -0400

    Save this

---
+++ javascript_101/03_eventos.js
@@ -0,0 +1,12 @@
+// var verifiacarDiasDelAño = "5 años";
+// document.write(verifiacarDiasDelAño);
+
+// Seleccionar la etiqueta
+var titulo1 = document.querySelector("h1");
+
+console.log(titulo1);
+
+// Registrar el evemto al elememto
+titulo1.addEventListener("click", function(){
+    open("https://www.google.es/");
+});
--- javascript_101/ejercicio.js
+++ javascript_101/ejercicio.js
@@ -1,36 +1,41 @@
 // Hacer un script que el usuario
-var nombre = prompt("Digite su nombre");
-var edad = prompt("Digite su edad");
+// var nombre = prompt("Digite su nombre");
+// var edad = prompt("Digite su edad");
 
-console.log("nombre");
-console.log("edad");
+// console.log("nombre");
+// console.log("edad");
 
-document.write(`<h1>${nombre}</h1>`);
-document.write(`<p>${edad}</p>`);
+// document.write(`<h1>${nombre}</h1>`);
+// document.write(`<p>${edad}</p>`);
 
 // 2. Hacer que el usuario digite su ciudad, barrio y direccion e imprimirlo en un <div>
 
-var ciudad = prompt("Dijite su ciudad");
-var barrio = prompt("Dijite su barrio");
-var direccion = prompt("Dijite su direccion");
+// var ciudad = prompt("Dijite su ciudad");
+// var barrio = prompt("Dijite su barrio");
+// var direccion = prompt("Dijite su direccion");
 
-console.log("ciudad");
-console.log("barrio");
-console.log("direccion");
+// console.log("ciudad");
+// console.log("barrio");
+// console.log("direccion");
 
-document.write(`<div>${ciudad}</div>`);
-document.write(`<div>${barrio}</div>`);
-document.write(`<div>${direccion}</div>`);
+// document.write(`<div>${ciudad}</div>`);
+// document.write(`<div>${barrio}</div>`);
+// document.write(`<div>${direccion}</div>`);
 
 // Ejercicio
 // Para mi y para alguien
 
 // Hacer que el pentago te diga su ID fudanmental e imprimirlo en <p>
 
-var pentago = prompt("Escriba la ID del pentagono");
+// var pentago = prompt("Escriba la ID del pentagono");
 
-console.log("pentagono")
+// console.log("pentagono");
 
-document.write(`<p>${pentagono}</p>`)
+// document.write(`<p>${pentagono}</p>`);
  
-
+var escrito = "Hola mundo";
+alert(escrito);
+var nombre = prompt("Escribe tu nombre");
+var edad = prompt("Escribe tu edad");
+document.write(`<p>Hola ${nombre}, tienes ${edad} años.</p>`);
+console.log(`<p>Hola ${nombre}, tienes ${edad} años.</p>`);
